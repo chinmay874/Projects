9a47b3a7249a8330cc0554e9bd1f5c9895a16bb6
chinmay jain <chinmay874@gmail.com>
2023-06-24 23:06:48 +0530

    UI commited

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Getting Started with Create React App
 
+
+Google Docs is an online word processor included as part of the free, web-based Google Docs Editors suite offered by Google.
+
+In this, I have develope UI of google Docs using react js. In this i have used react-icon.
+
+
+
 <!-- here we use react icon. -->
 
 <!-- install react-icon -->
--- src/Word/Word.jsx
+++ src/Word/Word.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './Word.module.css'
 import {FcDocument} from 'react-icons/fc';
 import { RxCounterClockwiseClock } from "react-icons/rx";
-import {AiOutlineStar,AiOutlineCaretDown,AiFillLock,AiOutlinePrinter,AiOutlineFormatPainter} from 'react-icons/ai';
-import {MdDriveFileMove,MdOutlineInsertComment} from 'react-icons/md';
-import {BsFillCloudCheckFill, BsCameraVideo} from 'react-icons/bs';
-import {BiUndo,BiRedo} from 'react-icons/bi';
+import {AiOutlineStar,AiOutlineCaretDown,AiFillLock,AiOutlinePrinter,AiOutlineFormatPainter,AiOutlineMinusSquare,
+        AiOutlinePlusSquare,AiOutlineHighlight,AiFillCaretDown} from 'react-icons/ai';
+import {MdDriveFileMove,MdOutlineInsertComment, MdInsertLink, MdOutlineAddComment,MdFormatLineSpacing,
+         MdChecklist,MdFormatListBulleted,MdFormatListNumbered,MdFormatIndentDecrease,MdFormatIndentIncrease,MdFormatClear,
+         MdEdit} from 'react-icons/md';
+import {BsFillCloudCheckFill, BsCameraVideo,BsTypeBold,BsTypeItalic,BsTypeUnderline} from 'react-icons/bs';
+import {BiUndo,BiRedo,BiImageAlt,BiAlignLeft} from 'react-icons/bi';
+import { TbTextColor } from "react-icons/tb";
+import { IoIosArrowUp } from "react-icons/io";
+import {useReactToPrint} from 'react-to-print';
 
 
 function Word() {
+  const [isBold, setIsBold] = useState(false);
+  const [isItalic, setIsItalic] = useState(false);
+  const [text, setText] = useState('');
+  const [zoomLevel, setZoomLevel] = useState(50);
 
-  //const menubutton=[{File:['New','Open','Make a copy','Share',"Email","Download",'Save As','Save']}]
+  const handleBoldClick = () => {
+    setIsBold(!isBold);
+  };
+  const handleItalicClick = () => {
+    setIsItalic(!isItalic);
+  };
+  const handleZoomChange = (event) => {
+    setZoomLevel(parseInt(event.target.value));
+  };
 
-  const handlefilename=()=>{
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
 
-  }
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+  const componentRef = useRef();
 
+  
 
   return (
     <div className={styles.wordfile_container}>
@@ -25,11 +49,11 @@ function Word() {
         </div>
         <div className={styles.nameandmenu}>
             <div className={styles.filenamemenubar}>
-                <div className={styles.filename} onClick={handlefilename}> Untitled document</div>
+                <div className={styles.filename}> Untitled document</div>
                 <div className={styles.topicons}>
-                  <span className={styles.icons}><AiOutlineStar className={styles.iconfilemenubar}/></span>
-                  <span className={styles.icons}><MdDriveFileMove className={styles.iconfilemenubar}/></span>
-                  <span className={styles.icons}><BsFillCloudCheckFill className={styles.iconfilemenubar}/></span>
+                  <span><AiOutlineStar className={styles.iconfilemenubar}/></span>
+                  <span><MdDriveFileMove className={styles.iconfilemenubar}/></span>
+                  <span><BsFillCloudCheckFill className={styles.iconfilemenubar}/></span>
                 </div>
         </div>
         <div className={styles.menubtn}>
@@ -59,11 +83,11 @@ function Word() {
 
         <div className={styles.tools}>
           <div className={styles.tool1}>
-          <span><BiUndo className={styles.undo}/></span>
-          <span><BiRedo className={styles.redo}/></span>
-          <span><AiOutlinePrinter className={styles.printer}/></span>
-          <span><AiOutlineFormatPainter className={styles.painter}/></span>
-          <select className={styles.zoom}>
+          <span className={styles.lnformat}><BiUndo className={styles.undo}/></span>
+          <span className={styles.lnformat}><BiRedo className={styles.redo}/></span>
+          <span className={styles.lnformat}><AiOutlinePrinter className={styles.printer} onClick={handlePrint}/></span>
+          <span className={styles.lnformat}><AiOutlineFormatPainter className={styles.painter}/></span>
+          <select className={styles.zoom} onChange={handleZoomChange} >
             <option>50%</option>
             <option>75%</option>
             <option>90%</option>
@@ -88,21 +112,55 @@ function Word() {
             </select>
             </div>
             <div className={styles.fontsize}>
-              <button className={styles.decrement}>-</button>
-              <p>
-                
+           < AiOutlineMinusSquare className={styles.decrement}/>
+              <p className={styles.Fontsize} >
+                  20
               </p>
+              <AiOutlinePlusSquare className={styles.increment} />
             </div>
+            <div className={styles.iconclass}>
+            <span className={styles.lnformat}><BsTypeBold className={styles.icons} onClick={handleBoldClick}>{isBold ? 'Remove Bold' : 'Add Bold'}</BsTypeBold></span>
+            <span className={styles.lnformat}><BsTypeItalic className={styles.icons} onClick={handleItalicClick}>{isItalic ? 'Remove Italic' : 'Add Italic'}</BsTypeItalic></span>
+            <span className={styles.lnformat}><BsTypeUnderline className={styles.icons}/></span>
+            <span className={styles.lnformat}><TbTextColor className={styles.icons}/></span>
+            <span className={styles.lnformat}><AiOutlineHighlight className={styles.icons}/></span>
+            </div>
+            <div className={styles.image}>
+              <span className={styles.lnformat}><MdInsertLink className={styles.icons}/></span>
+              <span className={styles.lnformat}><MdOutlineAddComment className={styles.icons}/></span>
+              <span className={styles.lnformat}><BiImageAlt className={styles.icons}/></span>
+            </div>
+            <div className={styles.lineformat}>
+              <span className={styles.lnformat}><BiAlignLeft className={styles.icons}/><AiFillCaretDown className={styles.down}/></span>
+              <span className={styles.lnformat}><MdFormatLineSpacing className={styles.icons}/></span>
+              <span className={styles.lnformat}><MdChecklist className={styles.icons}/><AiFillCaretDown className={styles.down}/></span>
+              <span className={styles.lnformat}><MdFormatListBulleted className={styles.icons}/><AiFillCaretDown className={styles.down}/></span>
+              <span className={styles.lnformat}><MdFormatListNumbered className={styles.icons}/><AiFillCaretDown className={styles.down}/></span>
+              <span className={styles.lnformat}><MdFormatIndentDecrease className={styles.icons}/></span>
+              <span className={styles.lnformat}><MdFormatIndentIncrease className={styles.icons}/></span>
+              <span className={styles.lnformat}><MdFormatClear className={styles.icons}/></span> 
+            </div>
+            <div className={styles.rightside}>
+            <span><MdEdit className={styles.icons}/></span><span><AiFillCaretDown className={styles.down}/></span>
+            </div>
+            <IoIosArrowUp className={styles.icons}/>
         </div>
 
 
 
         <div className={styles.textareacontainer}>
-            <textarea className={styles.inputfield}>
-            </textarea>
+            <textarea className={styles.inputfield} 
+              contentEditable={true}
+              ref={componentRef}
+              value={text}
+              onChange={handleTextChange} 
+              style={{
+              fontWeight: isBold ? 'bold' : 'normal',
+              fontStyle: isItalic ? 'italic' : 'normal',
+              transform: `scale(${zoomLevel / 100})`,
+              }}>
+            </textarea> 
         </div>
-        
-
     </div>
   )
 }
